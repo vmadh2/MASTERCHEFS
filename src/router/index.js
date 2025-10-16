c94c59e0eb73ca9914d713ed2210f341921970dc
lilychen88 <lilyc881217@gmail.com>
2025-10-17 06:09:34 +1000

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeBoard  from '../components/HomeBoard.vue'
-import PrivateBoard from '../components/PrivateBoard.vue'
-import FormSheet from '@/components/FormSheet.vue'
-import TestPage from '@/components/TEST.vue'
+import BulletinBubbles from '@/components/BulletinBubbles.vue'
+
+const routes = [
+  { path: '/', name: 'Huddle', component: BulletinBubbles },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home-board',
-      component: HomeBoard
-    },
-    {
-      path: '/formsheet',
-      name: 'form-sheet',
-      component: FormSheet
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: TestPage
-    },
-    { path: '/privateboard', 
-      name: 'private-board', 
-      component: PrivateBoard 
-    },
-  ],
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
