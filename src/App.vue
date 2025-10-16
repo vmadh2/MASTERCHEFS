d7042c32d568c2434dc7f8908f36c7361164eb76
lilychen88 <lilyc881217@gmail.com>
2025-10-17 06:09:16 +1000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
-  <div id="app">
-    <!-- router-view renders the component for the current route -->
-    <router-view />
-  </div>
+  <router-view />
 </template>
 
-<script>
-export default {
-  name: 'App',
-};
+<script setup>
+// nothing else needed
 </script>
 
 <style>
-body, html {
-  margin: 0;
-  padding: 0;
-  background-color: #747070; /* Change this to your desired color */
-}
+/* global styles */
+html, body, #app { height: 100%; margin: 0; }
 </style>
